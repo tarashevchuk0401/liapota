f39d718c1cd5ef2727bcda5631b048689c8a9d36
Taras Shevchuk <tarashevchuk0401@gmail.com>
2023-07-07 19:45:16 +0200

    sStage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'liapota';
   viberIconToogle: boolean = true;
+
 
   closeViberIcon(){
     this.viberIconToogle = false;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('ua');
+    translate.use('ua');
+  }
+
+  switchLanguage(language: string){
+    this.translate.use(language)
+  }
 
 }
